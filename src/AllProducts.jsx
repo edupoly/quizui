1d3c9e76dba1aedf414e62d01be834e79c58f57e
edupoly <info.edupoly@gmail.com>
2022-09-28 21:03:20 +0530

    product details

---
+++ src/AllProducts.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function AllProducts() {
+  const [products, setproducts] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:4500/getAllProducts")
+    .then(res=>res.json())
+    .then(products=>setproducts(products))
+  },[])
+  return (
+    <div className='d-flex flex-wrap justify-content-between'>
+      {
+        products.map((product,i)=>{
+          return <div className='w-25 border p-2'>
+            <img src={product.image} className="img-thumbnail img-fluid" alt="" />
+            <h5>
+              <Link to={`/productDetails/${product['_id']}`}>{product.title}</Link>
+            </h5>
+            </div>
+        })
+      }
+    </div>
+  )
+}
+
+export default AllProducts
--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Quiz from './Quiz';
 import AddProduct from './AddProduct';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <AddProduct></AddProduct>
-      <Quiz></Quiz>
+      <Navbar></Navbar>
+      <div className='container'>
+        <Outlet></Outlet>
+      </div>
+      
     </div>
   );
 }
